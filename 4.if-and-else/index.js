ef5ddc4f61fa37a664e3e8226d8bd087b7b78cf1
Igor <igormatheusmarianodasilva@gmail.com>
2025-03-26 19:33:17 -0300

    Add files via upload

---
+++ 4.if-and-else/index.js
@@ -0,0 +1,15 @@
+let color = "green";  //Armazena um valor usando "let", que é uma variável de bloco
+
+if (color === "green") { //Verifica se a variável "color" tem o valor "green"
+    
+    console.log("Go");  //Se a condição for verdadeira, exiba no console a mensagem
+
+} else if (color === "yellow") {
+
+    console.log("Attention"); //Se a condição for verdadeira, exiba no console a mensagem
+
+} else if (color === "red") {
+
+    console.log("Stop") //Se a condição for verdadeira, exiba no console a mensagem
+
+}
