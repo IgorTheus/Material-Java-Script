1b9b12359db50bcba2304a20a2c129953c729662
Igor <igormatheusmarianodasilva@gmail.com>
2025-03-26 19:39:27 -0300

    Add files via upload

---
+++ 5.switch/index.js
@@ -0,0 +1,20 @@
+let color = "green";  //Armazena um valor usando "let", que é uma variável de bloco
+
+switch (color) {   
+
+    case "green": 
+        console.log("Go");
+        break;
+
+    case "yellow": 
+        console.log("Attention");
+        break;
+
+    case "red": 
+        console.log("Stop");
+        break;
+
+    default:
+        console.log("Invalid sign");
+
+}
